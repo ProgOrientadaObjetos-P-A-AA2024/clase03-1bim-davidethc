95c045d682cee44ef04021739d5d6ea50e99540f
davide <davidecondet@gmail.com>
2024-04-25 18:23:14 -0500

    25-04-24

--- EjerciciosClase/src/paquete1/InstitucionEducativa.java
+++ EjerciciosClase/src/paquete1/InstitucionEducativa.java
@@ -17,4 +17,47 @@ public class InstitucionEducativa {
     private int numeroSedes;
     
     /*Crear los método necesarios*/
+// Métodos establecer (set) para cada atributo
+    
+    public void establecerNombre(String c) {
+        nombre = c;
+    }
+    
+    public void establecerTipoInstitucion(String c) {
+    tipoInstitucion = c;
+    }
+    
+    public void establecerNumeroAlumnos(int c) {
+     numeroAlumnos =c;
+    }
+    
+    public void establecerNumeroDocentes(int c) {
+        numeroDocentes = c;
+    }
+    
+    public void establecerNumeroSedes(int c) {
+       numeroSedes = c;
+    }
+    
+    // Métodos obtener (get) para cada atributo
+    
+    public String obtenerNombre() {
+        return nombre;
+    }
+    
+    public String obtenerTipoInstitucion() {
+        return tipoInstitucion;
+    }
+    
+    public int obtenerNumeroAlumnos() {
+        return numeroAlumnos;
+    }
+    
+    public int obtenerNumeroDocentes() {
+        return numeroDocentes;
+    }
+    
+    public int obtenerNumeroSedes() {
+        return numeroSedes;
+    }
 }
--- EjerciciosClase/src/paquete2/Principal.java
+++ EjerciciosClase/src/paquete2/Principal.java
@@ -12,11 +12,25 @@ import paquete1.InstitucionEducativa;
  * @author reroes
  */
 public class Principal {
+
     public static void main(String[] args) {
+
         /*Un objeto de tipo InstitucionEducativa*/
-        
         InstitucionEducativa i1 = new InstitucionEducativa();
+        String nombre = "Daniel Alvarez Burneo";
+        String tipoInstitucion = "Privada";
+        int numeroAlumnos = 200;
+        int numeroDocentes = 30;
+        int numeroSedes = 3;
         
+        i1.establecerTipoInstitucion(tipoInstitucion);
+        i1.establecerNombre(nombre);
+        i1.establecerNumeroAlumnos(numeroAlumnos);
+        i1.establecerNumeroDocentes(numeroDocentes);
+        i1.establecerNumeroSedes(numeroSedes);
+
         // System.out.printf("%s     ", i1.);
+         System.out.printf("Institucion Educativa\n\naTipo de institucion:%s\n Nombres Completos: %s\n"
+                + "Numero de Alumnos%s\n "+"Numero de Alumnos%s\n"+"Numero de sedes %s\n",i1.obtenerTipoInstitucion(),i1.obtenerNombre(),i1.obtenerNumeroAlumnos(),i1.obtenerNumeroDocentes(),i1.obtenerNumeroSedes());
     }
 }
